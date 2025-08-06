a1e225f1f752cee36ec51617711609c298d80dc9
Mithu-Vulcan <mithushanthjonathanparrot@gmail.com>
2025-08-06 10:59:40 +0530

    removed the / in href from cards

--- scripts/components/card.js
+++ scripts/components/card.js
@@ -7,7 +7,7 @@ class CustomCard extends HTMLElement {
 
 	// Helper function to decode HTML entities
 	decodeHtmlEntities(str) {
-		const textarea = document.createElement('textarea');
+		const textarea = document.createElement("textarea");
 		textarea.innerHTML = str;
 		return textarea.value;
 	}
@@ -17,13 +17,15 @@ class CustomCard extends HTMLElement {
 		if (!config || !config.nav) return;
 
 		// Decode the HTML entities when reading attributes
-		const name = this.decodeHtmlEntities(this.getAttribute("name") || "Default name");
+		const name = this.decodeHtmlEntities(
+			this.getAttribute("name") || "Default name"
+		);
 		const price = this.getAttribute("price") || 4000;
 		const percentage = this.getAttribute("percentage") || 30;
 		const units = this.getAttribute("units") || 50;
 		const path = this.decodeHtmlEntities(
 			this.getAttribute("path") ||
-			"assets/men/DKDC College Eagles Long Sleeve Blue T-Shirt.webp"
+				"assets/men/DKDC College Eagles Long Sleeve Blue T-Shirt.webp"
 		);
 
 		const nowPrice = (price * (100 - percentage)) / 100;
@@ -47,12 +49,12 @@ class CustomCard extends HTMLElement {
 			</div>
         `;
 
-        const cardPriceDiv = `
+		const cardPriceDiv = `
             <div class="cardPriceDiv">
 				<p class="cardPrice">Rs.${price}.00</p>
 			</div>
         `;
-		
+
 		console.log(path);
 
 		const htmlString = `
@@ -67,7 +69,7 @@ class CustomCard extends HTMLElement {
 						<p class="cardDesc">${name}</p>
 						${disc ? cardPriceDiscDiv : cardPriceDiv}
 						<p class='cardUnits ${red ? "less" : ""}'>Only ${units} left</p>
-						<a href="/product.html"><p class="cardBuy">Buy Now</p></a>
+						<a href="product.html"><p class="cardBuy">Buy Now</p></a>
 					</div>
 				</div>
         `;
